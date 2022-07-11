bf01bb2afc0933b2e6a9e90f826d846dd7c5b704
Greg Turner <codeturner@gmail.com>
2022-07-11 17:43:53 -0600

    add user fixes

--- app/routes/admin/index.tsx
+++ app/routes/admin/index.tsx
@@ -3,6 +3,7 @@ import type { LoaderFunction, MetaFunction } from "@remix-run/server-runtime";
 import { initializeApp } from "firebase/app";
 import type { AuthError } from "firebase/auth";
 import { getAuth, sendSignInLinkToEmail } from "firebase/auth";
+import { useImmer } from "use-immer";
 import { verifySessionContext } from "~/utils/session/session.server";
 import type { User } from "~/utils/user-admin.server";
 import { listUsers } from "~/utils/user-admin.server";
@@ -30,13 +31,28 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function Index() {
   const users: User[] = useLoaderData() || {};
-  const handleVerify = async (email?: string) => {
+  const [state, setState] = useImmer<{ verifying?: boolean; error?: string }>({});
+  const { error, verifying } = state;
+
+  const handleVerify = (email?: string) => {
     if (email) {
-      try {
-        await sendSignInLinkToEmail(auth, email, { url: `${self.origin}/login`, handleCodeInApp: true });
-      } catch (err) {
-        console.log((err as AuthError).message);
-      }
+      setState(draft => {
+        draft.error = "";
+        draft.verifying = true;
+      });
+      sendSignInLinkToEmail(auth, email, { url: `${self.origin}/login`, handleCodeInApp: true })
+        .then(() => {
+          setState(draft => {
+            draft.error = `sent verification email to ${email}`;
+            draft.verifying = false;
+          });
+        })
+        .catch(err => {
+          setState(draft => {
+            draft.verifying = true;
+            draft.error = (err as AuthError).message;
+          });
+        });
     }
   };
 
@@ -63,13 +79,22 @@ export default function Index() {
                   <td className="px-4 py-3 ">{displayName || "None"}</td>
                   <td className="px-4 py-3 ">{email}</td>
                   <td className="px-4 py-3 ">{disabled ? "True" : "False"}</td>
-                  <td className="px-4 py-3 ">
+                  <td className="px-4 py-3 flex space-x-3 items-center">
                     {emailVerified ? (
                       "True"
                     ) : (
-                      <button className="text-blue-600 hover:text-blue-500" onClick={() => handleVerify(email)}>
-                        Send Link
-                      </button>
+                      <>
+                        <span>False</span>
+                        <span className={verifying ? "cursor-wait" : "cursor-pointer"} onClick={() => handleVerify(email)}>
+                          <svg className="inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18">
+                            <path fill="none" d="M0 0h24v24H0z" />
+                            <path
+                              d="M22 20.007a1 1 0 0 1-.992.993H2.992A.993.993 0 0 1 2 20.007V19h18V7.3l-8 7.2-10-9V4a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v16.007zM4.434 5L12 11.81 19.566 5H4.434zM0 15h8v2H0v-2zm0-5h5v2H0v-2z"
+                              fill="rgba(92,92,92,1)"
+                            />
+                          </svg>
+                        </span>
+                      </>
                     )}
                   </td>
                 </tr>
@@ -77,6 +102,7 @@ export default function Index() {
             </tbody>
           </table>
         </div>
+        <div className="flex items-center justify-center mt-4">{error && <p>{error}</p>}</div>
         <div className="flex items-center justify-center mt-4">
           <Link to="add" className="btn">
             Add User
